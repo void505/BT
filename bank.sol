3d332f9ab3bb573e8fbc19eacbae71104146abe5
void505 <168215272+void505@users.noreply.github.com>
2024-11-12 20:14:44 +0530

    bank.sol

--- bank.sol
+++ bank.sol
@@ -1,17 +1,20 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 contract Bank {
-   address public owner;
-   uint public balance;
-   constructor() {
-     owner = msg.sender;
+
+  mapping (address => uint256) balances;
+
+  function depositeMoney(uint256 amount) public  {
+    require(amount >= 0, "Amount must be greater than 0");
+    balances[msg.sender] = balances[msg.sender] + amount;
+  }
+
+  function withdrawMoney(uint256 amount) public  {
+    require(amount <= balances[msg.sender], "Insufficient balance");
+    balances[msg.sender] = balances[msg.sender] - amount;
+  }
+   
+   function showBalance() public view returns (uint256){
+    return balances[msg.sender];
    }
-   function deposit() external payable {
-     balance += msg.value;
-   }
-   function withdraw(uint _amount) external {     
-     require(balance >= _amount, "Insufficient balance!");
-     payable(owner).transfer(_amount);
-     balance -= _amount;
-   }
 }
